c421206d98965f78b83e616469f70b37e6347ce9
RamuNalla <nallaramu4321@gmail.com>
2025-10-16 07:41:59 +0530

    config file, requirements, src folder

---
+++ src/embeddings/base_embedding.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+from typing import List, Tuple, Optional
+import numpy as np
+
+"""
+Base class for word embedding models.
+Defines the interface that all embedding implementations must follow.
+"""
+
